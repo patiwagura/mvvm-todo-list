4dcb6b947dbfe02a816335ef69a7429cf52125e4
pato <patiwagura@gmail.com>
2023-10-30 10:41:31 +0300

    first commit - created mvvm-todo-list project

---
+++ app/src/main/java/com/cobiztech/mvvmtodo/data/Task.kt
@@ -0,0 +1,22 @@
+package com.cobiztech.mvvmtodo.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    val name: String,
+    val important: Boolean = false,
+    val completed: Boolean = false,
+    val created: Long = System.currentTimeMillis(),
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+) : Parcelable {
+    // format date from long-timeMillis to d/m/y
+    val createdDateFormatted: String
+        get() = DateFormat.getDateTimeInstance().format(created)
+
+}
